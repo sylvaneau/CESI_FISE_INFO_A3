481eadf81aefaa05880196cb5c82b25ae1eeb40e
sylva <sylvain.lavabre@live.fr>
2022-12-09 10:18:50 +0100

    Socket WS part 1

---
+++ WS_Socket/WS_Socket.Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace WS_Socket.Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocker = Connect();
+        }
+
+        private static Socket Connect()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 9000);
+            
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(endPoint);
+            socket.Listen();
+
+            return socket;
+        }
+
+        private static Socket AcceptConnection(Socket serverSocket)
+        {
+            Socket clientSocket = serverSocket.Accept();
+
+            if (clientSocket.RemoteEndPoint is IPEndPoint clientEndpoint)
+            {
+                //IPEndPoint clientEndpoint = (IPEndPoint)clientSocket.RemoteEndPoint;
+                Console.WriteLine("Client connected:" + clientEndpoint.Address.ToString() + ":" + clientEndpoint.Port);
+            }
+
+            return clientSocket;
+        }
+    }
+}
